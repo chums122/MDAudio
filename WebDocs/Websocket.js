ba60503860d0c40cedfeaa2f2948e405f7ea1ccb
chums122 <chums122@gmail.com>
2020-04-13 18:23:45 -0700

    Added Html

---
+++ WebDocs/Websocket.js
@@ -0,0 +1,35 @@
+var name = window.location
+document.session.name.value = name
+
+var text = document.session.name.value
+
+var ws = new WebSocket("ws://" + window.location.hostname + ":8887/");
+
+ws.onopen = function () {
+
+    if (name != null) {
+        document.write("Connected to websocket server! <br>");
+        ws.send("name:" + delineate(text));
+        document.write("Sent data: name:" + delineate(text) + "<br>");
+    }
+
+};
+
+ws.onmessage = function (evt) {
+    var sound = new Howl({
+        urls: ['sounds/' + evt.data + '.ogg']
+    }).play();
+};
+
+ws.onclose = function () {
+    alert("Closed!");
+};
+
+ws.onerror = function (err) {
+    alert("Error: " + err);
+};
+
+function delineate(str) {
+    theleft = str.indexOf("=") + 1;
+    theright = str.lastIndexOf("&");
+    return (str.substring(theleft, theright));
